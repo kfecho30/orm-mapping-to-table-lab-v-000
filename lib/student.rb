8c272362f189e4e730130f4f4cad0d70a9c788b6
Kelsey Fecho <kfecho30@gmail.com>
2018-03-30 12:47:58 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,6 +32,6 @@ class Student
       VALUES (?, ?);
     SQL
     DB[:conn].execute(sql, @name, @grade)
-    @id = DB[:conn].execute("SELECT last_inserted_rowid() FROM students;")
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students;")
   end
 end
