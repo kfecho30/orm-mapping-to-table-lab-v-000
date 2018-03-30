1f9bad6b69c21f018491e183c44a142add2dc829
Kelsey Fecho <kfecho30@gmail.com>
2018-03-30 12:46:06 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -26,4 +26,10 @@ class Student
     DB[:conn].execute(sql)
   end
 
+  def save
+    sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?, ?);
+    SQL
+    DB[:conn].execute(sql, @name, @grade)
 end
